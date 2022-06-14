f913be397b71b6df28f03946c860903c0af98f75
lebrancconvas <lebrancconvas@gmail.com>
2022-06-14 07:40:15 +0700

    :hammer: Create API Server.

--- api/main.go
+++ api/main.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import(
+	"fmt"
+	"net/http"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+type User struct {
+	ID uint64 `json:"id"`
+	Firstname string `json:"firstname"`
+	Lastname string `json:"lastname"`
+	Age uint64 `json:"age"`
+	Email string `json:"email"`
+	Tel string `json:"tel"` 
+}
+
+var data = []User{
+	{
+		ID: 1,
+		Firstname: "Test",
+		Lastname: "User",
+		Age: 12,
+		Email: "test@user.com",
+		Tel: "0819991199",
+	},
+}
 
 func main() {
 	fmt.Println("Test Server.")
+
+	router := gin.Default()
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"http://localhost:3000/"},
+		AllowMethods: []string{"GET", "POST"},
+		AllowHeaders: []string{"Content-Type", "Origin"},
+		ExposeHeaders: []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
+	
+	router.GET("/users", getUser)
+	router.Run(":8000")
+}
+
+func getUser(c *gin.Context) {
+	c.JSON(http.StatusOK, data)
 }
--- web/pages/testform/display.vue
+++ web/pages/testform/display.vue
@@ -1,5 +1,35 @@
 <template>
   <div>
-    <h1>Display</h1> 
+    <h1>Display</h1>
+    <div>
+      <div>{{ userData.firstname }}</div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+    <button @click="getData">Show Data</button>
   </div>
 </template>
+
+<script>
+  import axios from 'axios';
+  export default {
+    name: "Display",
+    data() {
+      return {
+        userData: []
+      }
+    },
+    methods: {
+      getData() {
+        axios.get("http://localhost:8000/users")
+          .then(res => this.userData === res.data)
+      }
+    },
+    async asyncData({ $axios }) {
+      const userData = await $axios.$get('http://localhost:8000/users')
+      return {userData}
+    }
+  }
+</script>
